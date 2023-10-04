28ff44f73c2debb61e3fddba2aeace71c49cf63a
prakash_vtech <prakash.adhikari@varosatech.com>
2023-10-04 17:49:00 +0545

    test commit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import Footer from '../components/Footer'
-import Form from '../components/Form'
-import Heroimage2 from '../components/Heroimage2'
-import Navbar from '../components/Navbar'
+import React from "react";
+import Footer from "../components/Footer";
+import Form from "../components/Form";
+import Heroimage2 from "../components/Heroimage2";
+import Navbar from "../components/Navbar";
 
 const Contact = () => {
   return (
     <>
-<Navbar/>
-<Heroimage2 heading="CONTACT" text="Let's Connect."/>
-<Form/>
-<Footer/>
+      <Navbar />
+      <Heroimage2 heading="CONTACT" text="Let's Connect." />
+      <Form />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Contact
+//this is for testing
+
+export default Contact;
